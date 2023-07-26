81dd8c77ae0364f844dca7285c0f187324d0d20f
Isra Abdelmobdy <progisra491@gmail.com>
2023-07-26 19:42:30 +0300

    Add project files.

---
+++ Lab1Q1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab1Q1
+{
+    internal class Program
+    {
+        public static double Calculator(double x, double y, char op)
+        {
+            if (op == '+')
+                return x + y;
+            else if (op == '-')
+                return x - y;
+            else if (op == '*')
+                return x * y;
+            else
+                return x / y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter operation (+ - * /) : ");
+            char op = Convert.ToChar(Console.ReadLine());
+            double result = Calculator(a, b, op);
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Lab1Q2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab1Q2
+{
+    internal class Program
+    {
+        public static int[] GetFactors(int num)
+        {
+            int[] factors = new int[num / 2];
+            int j = 0;
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                    factors[j++] = i;
+            }
+            return factors;
+        }
+
+        public static int SumOfFactors(int[] factors)
+        {
+            int sum = 0;
+            for (int i = 0; i < factors.Length; i++)
+            {
+                sum += factors[i];
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                var factors = GetFactors(i);
+                int sumOfFactors = SumOfFactors(factors);
+                if (sumOfFactors == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
